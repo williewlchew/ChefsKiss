00da32e6fcab638b9db7f76ec5f02aa50a33f418
Willie Chew <37847148+williewlchew@users.noreply.github.com>
2022-09-17 15:26:12 -0600

    merged in a comment, oops

--- Assets/Player/PlayerCollider.cs
+++ Assets/Player/PlayerCollider.cs
@@ -10,7 +10,7 @@ public class PlayerCollider : MonoBehaviour
     {
         if (collision.gameObject.tag == "Enemy")
         {
-            //playerManager.TakeDamage();
+            playerManager.TakeDamage();
         }
     }
 }
